8e052a5f0ed814bb60aaa4e32e2801463f7f91ec
SaltMaster <mathieu.kergoat@ensta-bretagne.org>
2020-09-28 21:56:48 +0200

    [holtrop] Holtrop-Mennen coefficients

--- Part.py
+++ Part.py
@@ -4,6 +4,12 @@ form_factor["rudder_behind_stern"] = [1.3,1.5]
 form_factor["twin_screw_balance_rudders"] = [2.8]
 form_factor["shaft_brackets"] = [3.0]
 form_factor["skeg"] = [1.5, 2.0]
+form_factor["strut_bossings"] = [3.0]
+form_factor["hull bossings"] = [2.0]
+form_factor["shafts"] = [2.0, 4.0]
+form_factor["stabilizer_fins"] = [2.8]
+form_factor["dome"] = [2.7]
+form_factor["bilge_keels"] = [1.4]
 
 class Part:
 	def __init__(self):
@@ -21,7 +27,16 @@ class Hull(Part):
 		self.LOA = 0.0
 		self.BOA = 0.0
 		self.D = 0.0
-	
+        self.CM = 0
+        self.CB = 0
+        self.ABT = 0
+        self.C_WP
+        
+        
+        
+    def computeWettedArea(self):
+        return 'to do'
+        
 class Appendage(Part):
 	def __init__(self):
 		Part.__init__(self)
--- holtrop_mennen.py
+++ holtrop_mennen.py
@@ -5,27 +5,42 @@ from collections import OrderedDict
 
 ## HOLTROP - MENNEN SHIP RESISTANCE EVALUATION
 
-def LRoL(Cp, LCB):
+def LCB_from_amidshipsoL(LCB, L):
+    """
+    Longitudinal center of buoyancy position measured from amidships as a 
+    percentage of waterline length L
+    """
+    return (LCB - 0.5*L)/L
+
+def LRoL(Cp, LCB, L):
     """
     Parameter used by the Holtrop approximation
     Cp : prismatic coefficient
     LCB: longitudinal position of the center of buoyancy
     """
-    return 1 - Cp + 0.06 * Cp * LCB * (4 * Cp - 1)
+    return 1 - Cp + 0.06 * Cp * LCB_from_amidshipsoL(LCB, L) * (4 * Cp - 1)
 
-def hull_form_factor(B, LCB, Cp)
+def hull_form_factor(T, L, B, LCB, Cp, C_stern):
     """
     Computes the hull form factor
     B: ship max width
     LCB: longitudinal position of center of buoyancy
     Cp: Prismatic coefficient
+    T: average moulded draft
     """
-    c12 = 
+    LCB_midoL = LCB_from_amidshipsoL(LCB,L)
+    LR = L * LRoL(Cp, LCB_midoL)
+    if T/L > 0.05:
+        c12 = (T/L)**0.2228446
+    elif T/L < 0.02:
+        c12 = 0.479948
+    else:
+        c12 = 48.20 * ((T / L) - 0.02) ** 2.078 + 0.479948
     c13 = 1 + 0.003 * C_stern
     
-    return c13 * (0.93 + c12 * (B / LR)**0.92497 * (0.95 - Cp)**(-0.521448) * (1 - Cp + 0.0225 * LCB)**(0.6906))
+    return c13 * (0.93 + c12 * (B / LR) ** 0.92497 * (0.95 - Cp)**(-0.521448) * (1 - Cp + 0.0225 * LCB_midoL)**(0.6906))
 
-def Re(V,L,rho,mu)
+def Re(V,L,rho,mu):
     """
     Computes the Reynolds number for the flow
     around the ship or element
@@ -35,7 +50,7 @@ def Re(V,L,rho,mu)
     """
     return rho * V * L / mu
 
-def CF_ITTC57(Re)
+def CF_ITTC57(Re):
     """
     Skin friction coefficient computed
     using ITTC 57 Friction Line Formula
@@ -50,4 +65,4 @@ def makeDimensionnal(rho, V, S, C):
 
 
 
-RT = RF * (1+k1) + RAPP + RW + RB + RTR + RA
+#RT = RF * (1+k1) + RAPP + RW + RB + RTR + RA
